bb6c7126e80b89a88511629a7ec166dba8e4ee1e
1000pilar <nunua7a8@gmailcom>
2017-07-07 11:37:15 +0700

    connect bootstrap vue

--- front_end/components.js
+++ front_end/components.js
@@ -32,32 +32,49 @@
 Vue.component('love-headers', {
   props: [''],
   template: `
-  <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-    <el-menu-item index="1">Processing Center</el-menu-item>
-    <el-submenu index="2">
-      <template slot="title">Workspace</template>
-      <el-menu-item index="2-1">item one</el-menu-item>
-      <el-menu-item index="2-2">item two</el-menu-item>
-      <el-menu-item index="2-3">item three</el-menu-item>
-    </el-submenu>
-    <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">Orders</a></el-menu-item>
-    </el-menu>
-    <div class="line"></div>
-    <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-    <el-menu-item index="1">Processing Center</el-menu-item>
-    <el-submenu index="2">
-      <template slot="title">Workspace</template>
-      <el-menu-item index="2-1">item one</el-menu-item>
-      <el-menu-item index="2-2">item two</el-menu-item>
-      <el-menu-item index="2-3">item three</el-menu-item>
-    </el-submenu>
-    <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">Orders</a></el-menu-item>
-  </el-menu>`,
+  <b-navbar toggleable type="inverse" variant="success">
+
+    <b-nav-toggle target="nav_collapse"></b-nav-toggle>
+
+    <b-link class="navbar-brand" to="#">
+      <span>BootstrapVue</span>
+    </b-link>
+
+    <b-collapse is-nav id="nav_collapse">
+
+      <b-nav is-nav-bar>
+        <b-nav-item>Support</b-nav-item>
+        <b-nav-item>Docs</b-nav-item>
+        <b-nav-item>Contact Us</b-nav-item>
+      </b-nav>
+
+      <b-nav is-nav-bar class="ml-auto">
+
+        <!-- Navbar dropdowns -->
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item to="#">EN</b-dropdown-item>
+          <b-dropdown-item to="#">ES</b-dropdown-item>
+          <b-dropdown-item to="#">RU</b-dropdown-item>
+          <b-dropdown-item to="#">FA</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+
+          <!-- Using button-content slot -->
+          <template slot="button-content">
+            <span style="font-weight: bold;">User</span>
+          </template>
+
+          <b-dropdown-item to="#">Profile</b-dropdown-item>
+          <b-dropdown-item to="#">Signout</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+      </b-nav>
+    </b-collapse>
+</b-navbar>`,
   data() {
     return {
-      activeIndex: '1',
-      activeIndex2: '1'
-    };
+    }
   },
   methods: {
     handleSelect(key, keyPath) {
